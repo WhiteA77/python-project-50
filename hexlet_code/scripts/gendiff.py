22703de4fb895487c987c12ee108fc0c0cba0e73
WhiteA <crazy032@mail.ru>
2025-04-15 12:22:06 +0300

    eighth lesson

--- hexlet_code/scripts/gendiff.py
+++ hexlet_code/scripts/gendiff.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 from hexlet_code.parsers import parse_file
 from hexlet_code.diff_builder import build_diff
 from hexlet_code.formatters.stylish import format_stylish
+from importlib import import_module
 
 
 def generate_diff(file_path1, file_path2, format_name='stylish'):
@@ -9,9 +10,11 @@ def generate_diff(file_path1, file_path2, format_name='stylish'):
     data2 = parse_file(file_path2)
     diff = build_diff(data1, data2)
     
-    if format_name == 'stylish':
-        return format_stylish(diff)
-    raise ValueError(f"Unknown format: {format_name}")
+    try:
+        formatter_module = import_module(f'hexlet_code.formatters.{format_name}')
+        return formatter_module.format_plain(diff) if format_name == 'plain' else formatter_module.format_stylish(diff)
+    except ImportError:
+        raise ValueError(f"Unknown format: {format_name}")
 
 
 def main():

347b54abff901399108e4a21ad1717cb193b581e
WhiteA <crazy032@mail.ru>
2025-04-18 11:43:51 +0300

    edit ci.yml, plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,11 +1,11 @@
 def format_value(value):
     if isinstance(value, dict):
-        return "[complex value]"
-    elif isinstance(value, bool):
+        return '[complex value]'
+    if isinstance(value, bool):
         return str(value).lower()
-    elif value is None:
-        return "'null'"
-    elif isinstance(value, str):
+    if value is None:
+        return 'null'
+    if isinstance(value, str):
         return f"'{value}'"
     return str(value)
 
